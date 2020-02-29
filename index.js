bf33656708bbc0411f82656306a2899b2646ad25
YUKIKO IKEDA <applelele@gmail.com>
2020-02-29 23:41:51 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,49 @@
+function add() {
+  var n = 5;
+  n += 3;
+}
+
+function subtract() {
+  var n = 5;
+  n -= 2;
+}
+
+function multiple() {
+  var n = 5;
+  n *= 6;
+}
+function devide() {
+  var n = 5;
+  n /= 5;
+}
+
+function add(a, b) {
+  var addNumber = a + b;
+  return addNumber;
+}
+
+function subtract(a, b) {
+  var subNumber = a - b;
+  return subNumber;
+}
+
+function multiply(a, b) {
+  var multiNumber = a * b;
+  return multiNumber;
+}
+
+function divide(a, b) {
+  var divNumber = a / b;
+  return divNumber;
+}
+
+}
+function increment(n) {
+  var increNumber = n ++;
+  return increNumber;
+}
+
+function decrement(n) {
+  var decreNumber = n --;
+  return decreNumber;
+}
